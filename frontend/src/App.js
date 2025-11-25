91b499acf2d91078b2cc4efd6b4d270b449fe044
Phung Nhat <phungnhatfr2352@gmail.com>
2025-11-25 16:28:25 +0100

    Create pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
-    
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
