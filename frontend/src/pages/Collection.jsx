e1ca96eebbf6a7d8b6d064b04e1199c348000ee7
Phung Nhat <phungnhatfr2352@gmail.com>
2025-12-08 21:43:59 +0100

    Finish UI Product page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import { ShopContext } from '../context/ShopContext'
 
 const Collection = () => {
   
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -36,6 +36,10 @@ const Collection = () => {
   const applyFilter = () => {
   
     let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+    }
     
     if (category.length > 0) {
       productsCopy = productsCopy.filter(item => category.includes(item.category));
@@ -75,7 +79,7 @@ const Collection = () => {
   
   useEffect(() => {
     applyFilter()
-  },[category,subCategory])
+  },[category,subCategory, search, showSearch])
   
   return (
       <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
